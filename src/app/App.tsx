6139651540bb21f52843fd79efd8d06cff850586
Nazaruk-D <nazaruk-dima@mail.ru>
2022-10-16 20:02:55 +0300

    feat: add LineProgress and change Header styles

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,14 +2,19 @@ import React from 'react';
 import './App.css';
 import Header from "./header/Header";
 import Content from "./content/Content";
+import {useAppSelector} from "./store";
+import {RequestStatusType} from "./app-reducer";
+import {LinearProgress} from "@mui/material";
 
 function App() {
-  return (
-    <div className="App">
-        <Header/>
-        <Content/>
-    </div>
-  );
+    const status = useAppSelector<RequestStatusType>(store => store.app.status)
+    return (
+        <div className="App">
+            <Header/>
+            {status === "loading" && <LinearProgress/>}
+            <Content/>
+        </div>
+    );
 }
 
 export default App;
