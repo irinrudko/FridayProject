d084a99b8b1d1e1e0abc7eb8563d226fecdcf19a
Nazaruk-D <nazaruk-dima@mail.ru>
2022-10-16 13:50:50 +0300

    feat: creating a registration component

--- src/features/auth/registration/Registration.tsx
+++ src/features/auth/registration/Registration.tsx
@@ -1,11 +1,134 @@
-import React from 'react';
+import React, {useState} from 'react'
+import s from './Registration.module.css'
+import authStyle from '../../auth/Auth.module.css'
+import {NavLink} from 'react-router-dom'
+import {routes} from '../../../app/routes/Routes'
+import Grid from '@mui/material/Grid'
+import Checkbox from '@mui/material/Checkbox'
+import FormControl from '@mui/material/FormControl'
+import FormControlLabel from '@mui/material/FormControlLabel'
+import FormGroup from '@mui/material/FormGroup'
+import FormLabel from '@mui/material/FormLabel'
+import TextField from '@mui/material/TextField'
+import Button from '@mui/material/Button'
+import {useFormik} from 'formik'
+import {IconButton, Input, InputAdornment, InputLabel, OutlinedInput} from "@mui/material";
+import {Visibility, VisibilityOff} from "@mui/icons-material";
+
+//TODO: validation
 
 const Registration = () => {
-    return (
-        <div>
-            Register
-        </div>
-    );
-};
 
-export default Registration;
+    const [password, setPassword] = useState(false);
+    const [confirmPassword, setConfirmPassword] = useState(false);
+
+    const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+    };
+
+    const handleClickShowPassword = (type: "pass" | "confirm") => {
+
+        if (type === "pass") {
+            setPassword(!password)
+        } else {
+            setConfirmPassword(!confirmPassword)
+        }
+    };
+
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+            rememberMe: false,
+            confirmPassword: ''
+        },
+        onSubmit: (values) => {
+            // alert(JSON.stringify(values))
+        },
+    })
+
+    return (
+        <Grid container justifyContent={'center'}>
+            <Grid item justifyContent={'center'}>
+                <form onSubmit={formik.handleSubmit}>
+                    <FormControl className={authStyle.form}>
+                        <FormLabel>
+                            <h2 className={authStyle.title}>Sign up</h2>
+                        </FormLabel>
+                        <FormGroup>
+                            <TextField
+                                label="Email"
+                                name="email"
+                                margin="normal"
+                                variant="standard"
+                                className={authStyle.textField}
+                                onChange={formik.handleChange}
+                                value={formik.values.email}
+                            />
+                            <FormControl variant="standard" className={s.input}>
+                                <InputLabel htmlFor="password" >Password</InputLabel>
+                                <Input
+                                    id="password"
+                                    name="password"
+                                    type={password ? 'text' : 'password'}
+                                    onChange={formik.handleChange}
+                                    value={formik.values.password}
+                                    className={s.input}
+
+                                    endAdornment={
+                                        <InputAdornment position="end">
+                                            <IconButton
+                                                aria-label="toggle password visibility"
+                                                onClick={()=>handleClickShowPassword("pass")}
+                                                onMouseDown={handleMouseDownPassword}
+                                            >
+                                                {password ? <VisibilityOff /> : <Visibility />}
+                                            </IconButton>
+                                        </InputAdornment>
+                                    }
+                                />
+                            </FormControl>
+                            <FormControl variant="standard" className={s.lastInput}>
+                            <InputLabel htmlFor="confirmPassword">Confirm Password</InputLabel>
+                            <Input
+                                id="confirmPassword"
+                                name="confirmPassword"
+                                type={confirmPassword? 'text' : 'password'}
+                                onChange={formik.handleChange}
+                                value={formik.values.confirmPassword}
+                                endAdornment={
+                                    <InputAdornment position="end">
+                                        <IconButton
+                                            aria-label="toggle password visibility"
+                                            onClick={()=>handleClickShowPassword("confirm")}
+                                            onMouseDown={handleMouseDownPassword}
+                                        >
+                                            {confirmPassword ? <VisibilityOff /> : <Visibility />}
+                                        </IconButton>
+                                    </InputAdornment>
+                                }
+                            />
+                            </FormControl>
+                            <Button
+                                type={'submit'}
+                                variant={'contained'}
+                                color={'primary'}
+                                className={`${authStyle.button} ${s.button}`}
+                            >
+                                Sign Up
+                            </Button>
+                            <p className={authStyle.text}>Already have an account? </p>
+                            <div>
+                                <NavLink to={routes.login} className={authStyle.link}>
+                                    Sign In
+                                </NavLink>
+                            </div>
+                        </FormGroup>
+                    </FormControl>
+                </form>
+            </Grid>
+        </Grid>
+    )
+}
+
+export default Registration
