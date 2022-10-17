0023503233cc809f8b0a0a47215b04750d213f97
Iryna Rudzko <irinrudko@gmail.com>
2022-10-17 09:42:27 +0400

    feat: add loginTC

--- src/features/auth/login/login-reducer.ts
+++ src/features/auth/login/login-reducer.ts
@@ -1,10 +1,39 @@
-const initialState = {}
-type InitialStateType = typeof initialState
-type ActionType = any
+import { Dispatch } from 'redux'
+import { LoginParamsData, regAPI } from '../../../api/api'
+import { AxiosError } from 'axios'
 
-export const loginReducer = (state: InitialStateType = initialState, action: ActionType): InitialStateType => {
+const initialState = {
+    isLoggedIn: false,
+}
+
+export const loginReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
     switch (action.type) {
+        case 'login/SET-IS-LOGGED-IN':
+            return { ...state, isLoggedIn: action.isLoggedIn }
         default:
             return state
     }
 }
+
+export const loginTC = (data: LoginParamsData) => (dispatch: Dispatch<ActionsType>) => {
+    regAPI
+        .login(data)
+        .then((res) => {
+            dispatch(setIsLoggedInAC(true))
+        })
+        .catch((err: AxiosError) => {
+            //@ts-ignore
+            let error = err.response.data.error
+            if (error === 'not valid email/password /ᐠ-ꞈ-ᐟ\\') {
+                alert(error)
+            }
+            if (error === 'user not found /ᐠ-ꞈ-ᐟ\\') {
+                alert(error)
+            }
+        })
+}
+
+export const setIsLoggedInAC = (isLoggedIn: boolean) => ({ type: 'login/SET-IS-LOGGED-IN', isLoggedIn } as const)
+
+type InitialStateType = typeof initialState
+type ActionsType = ReturnType<typeof setIsLoggedInAC>
