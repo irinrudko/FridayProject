df59b61419f6eee137ce3f5a8e49173eb99f1024
Nazaruk-D <nazaruk-dima@mail.ru>
2022-10-17 10:06:06 +0300

    test: test for initialized AC

--- src/app/app-reducer.test.ts
+++ src/app/app-reducer.test.ts
@@ -1,10 +1,10 @@
-import {appReducer, RequestStatusType, setAppStatusAC, setErrAC} from "./app-reducer";
+import {appReducer, initializedAC, RequestStatusType, setAppStatusAC, setErrAC} from "./app-reducer";
 
 test('status should be changed', () => {
-
     let startState = {
         status: "loading" as RequestStatusType,
         error: null as null | string,
+        initialized: false
     }
 
     const action = setAppStatusAC("succeeded")
@@ -14,15 +14,29 @@ test('status should be changed', () => {
 });
 
 test('error message should be changed', () => {
-
     let startState = {
         status: "loading" as RequestStatusType,
         error: null as null | string,
+        initialized: false
     }
 
     const action = setErrAC("test message error")
     const endState = appReducer(startState, action)
 
     expect(endState.error).toBe("test message error");
+    expect(endState.status).toBe("loading");
+});
+
+test('initialized should be changed', () => {
+    let startState = {
+        status: "loading" as RequestStatusType,
+        error: null as null | string,
+        initialized: false
+    }
+
+    const action = initializedAC(true)
+    const endState = appReducer(startState, action)
+
+    expect(endState.initialized).toBe(true);
     expect(endState.status).toBe("loading");
 });
