2665e965f178c5ac2172dfff7650ee42fe91c0da
Iryna Rudzko <irinrudko@gmail.com>
2022-10-17 13:20:30 +0400

    feat: add loginTC and dispatch user to state

--- src/features/auth/login/login-reducer.ts
+++ src/features/auth/login/login-reducer.ts
@@ -1,39 +1,63 @@
-import { Dispatch } from 'redux'
-import { LoginParamsData, regAPI } from '../../../api/api'
-import { AxiosError } from 'axios'
+import { LoginParamsData, regAPI, UserData } from '../../../api/api'
+import { setAppStatusAC, setErrAC } from '../../../app/app-reducer'
+import { AppThunk } from '../../../app/store'
+
+type InitialStateType = {
+    isLoggedIn: boolean
+    user: UserData
+}
 
 const initialState = {
     isLoggedIn: false,
+    user: {
+        _id: '',
+        email: '',
+        name: '',
+        avatar: '',
+        publicCardPacksCount: 0,
+        created: null,
+        updated: null,
+        isAdmin: false,
+        verified: false,
+        rememberMe: false,
+        error: '',
+    },
 }
 
 export const loginReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
     switch (action.type) {
         case 'login/SET-IS-LOGGED-IN':
             return { ...state, isLoggedIn: action.isLoggedIn }
+        case 'SET-USER-DATA':
+            return { ...state, user: { ...action.user } }
         default:
             return state
     }
 }
 
-export const loginTC = (data: LoginParamsData) => (dispatch: Dispatch<ActionsType>) => {
-    regAPI
-        .login(data)
-        .then((res) => {
-            dispatch(setIsLoggedInAC(true))
-        })
-        .catch((err: AxiosError) => {
-            //@ts-ignore
-            let error = err.response.data.error
-            if (error === 'not valid email/password /ᐠ-ꞈ-ᐟ\\') {
-                alert(error)
-            }
-            if (error === 'user not found /ᐠ-ꞈ-ᐟ\\') {
-                alert(error)
-            }
-        })
-}
+export const loginTC =
+    (data: LoginParamsData): AppThunk =>
+    (dispatch) => {
+        dispatch(setAppStatusAC('loading'))
+
+        regAPI
+            .login(data)
+            .then((res) => {
+                dispatch(setIsLoggedInAC(true))
+                dispatch(setAppStatusAC('succeeded'))
+                alert('signed in')
+                dispatch(setUserDataAC(res))
+                console.log(res)
+            })
+            .catch((err: any) => {
+                let error = err.response.data.error
+                console.log(err)
+                dispatch(setErrAC(error))
+                dispatch(setAppStatusAC('failed'))
+            })
+    }
 
 export const setIsLoggedInAC = (isLoggedIn: boolean) => ({ type: 'login/SET-IS-LOGGED-IN', isLoggedIn } as const)
+export const setUserDataAC = (user: UserData) => ({ type: 'SET-USER-DATA', user } as const)
 
-type InitialStateType = typeof initialState
-type ActionsType = ReturnType<typeof setIsLoggedInAC>
+type ActionsType = ReturnType<typeof setIsLoggedInAC> | ReturnType<typeof setUserDataAC>
