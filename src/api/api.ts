7d5596a98b134b85e6d4307792e50b1a8b1e6ab5
Iryna Rudzko <irinrudko@gmail.com>
2022-10-17 09:42:02 +0400

    feat: add login post request

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,21 +1,46 @@
-import axios, {AxiosResponse} from "axios";
-
+import axios, { AxiosResponse } from 'axios'
 
 const instance = axios.create({
     baseURL: process.env.REACT_APP_BACK_URL || 'http://localhost:7542/2.0/',
     withCredentials: true,
 })
 
-
 export const regAPI = {
     authRegistration(email: string, password: string) {
-        return instance.post<RegistrationPayloadType, AxiosResponse<any>>(`auth/register`, {email, password})
+        return instance.post<RegistrationPayloadType, AxiosResponse<any>>(`auth/register`, { email, password })
+    },
+    login(data: LoginParamsData) {
+        return instance
+            .post<LoginParamsData, AxiosResponse<LoginResponseType>>('auth/login', data)
+            .then((response) => response.data)
     },
 }
 
 //types
 
 export type RegistrationPayloadType = {
-    email: string,
+    email: string
     password: string
+}
+
+export type LoginParamsData = {
+    email: string
+    password: string
+    rememberMe: boolean
+}
+
+type LoginResponseType = {
+    _id: string
+    email: string
+    rememberMe: boolean
+    isAdmin: boolean
+    name: string
+    verified: boolean
+    publicCardPacksCount: number
+    created: Date | string
+    updated: Date | string
+    __v: number
+    token: string
+    tokenDeathTime: number
+    avatar: string
 }
