3239a97acaeef447bad2b5eef8322b628617f58a
Iryna Rudzko <irinrudko@gmail.com>
2022-10-18 17:10:22 +0400

    feat: validation to ForgotPassword.tsx

--- src/features/auth/forgotPassword/ForgotPassword.tsx
+++ src/features/auth/forgotPassword/ForgotPassword.tsx
@@ -1,31 +1,42 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import s from './ForgotPassword.module.css'
 import authStyle from '../Auth.module.css'
-import {Navigate, NavLink, useNavigate} from 'react-router-dom'
-import {routes} from '../../../app/routes/Routes'
+import { Navigate, NavLink, useNavigate } from 'react-router-dom'
+import { routes } from '../../../app/routes/Routes'
 import Grid from '@mui/material/Grid'
 import FormControl from '@mui/material/FormControl'
 import FormGroup from '@mui/material/FormGroup'
 import FormLabel from '@mui/material/FormLabel'
 import TextField from '@mui/material/TextField'
 import Button from '@mui/material/Button'
-import {useFormik} from 'formik'
-import {useAppDispatch} from "../../../app/store";
-import {forgotPassword} from "./forgotPassword-reducer";
-
+import { useFormik } from 'formik'
+import { useAppDispatch } from '../../../app/store'
+import { forgotPassword } from './forgotPassword-reducer'
+import { FormikErrorType } from '../auth-types'
 
 export const ForgotPassword = () => {
     const dispatch = useAppDispatch()
     const [sendValue, setSendValue] = useState(false)
-    const navigate = useNavigate();
+    const navigate = useNavigate()
 
     const formik = useFormik({
         initialValues: {
             email: '',
         },
+        validate: (values) => {
+            const errors: FormikErrorType = {}
+
+            if (!values.email) {
+                errors.email = 'Email is required'
+            } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+                errors.email = 'Invalid email address'
+            }
+
+            return errors
+        },
         onSubmit: (values) => {
             dispatch(forgotPassword(values))
-            navigate(routes.passwordRecovery);
+            navigate(routes.passwordRecovery)
         },
     })
 
@@ -47,17 +58,14 @@ export const ForgotPassword = () => {
                                 onChange={formik.handleChange}
                                 value={formik.values.email}
                             />
+                            {formik.errors.email ? <div>{formik.errors.email}</div> : null}
+
                             <FormLabel>
                                 <p className={s.textInstruction}>
                                     Enter your email address and we will send you further instructions
                                 </p>
                             </FormLabel>
-                            <Button
-                                type={'submit'}
-                                variant={'contained'}
-                                color={'primary'}
-
-                            >
+                            <Button type={'submit'} variant={'contained'} color={'primary'}>
                                 Send Instructions
                             </Button>
                             <p className={authStyle.text}>Did you remember your password? </p>
@@ -73,5 +81,3 @@ export const ForgotPassword = () => {
         </Grid>
     )
 }
-
-
