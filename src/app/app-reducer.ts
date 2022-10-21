1499558a258f87b7f2c5f20fa530beffaeb6bdc1
Nazaruk-D <nazaruk-dima@mail.ru>
2022-10-21 21:34:11 +0300

    fix: commented StrictMode in index.tsx

--- src/app/app-reducer.ts
+++ src/app/app-reducer.ts
@@ -30,7 +30,6 @@ export const initializedAC = (initializedStatus: boolean) => ({ type: 'APP/INITI
 
 //ThunkC
 export const initializedTC = (): AppThunk => (dispatch) => {
-    // debugger
     dispatch(setAppStatusAC('loading'))
     dispatch(initializedAC(false))
     authAPI
