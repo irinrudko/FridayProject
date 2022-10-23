9257fd4b24d84be5d77d9cdf15ffe558cb9ce722
Iryna Rudzko <irinrudko@gmail.com>
2022-10-23 21:00:09 +0400

    feat: add packs-reducer (actions and thunks)

--- src/features/cards/cards-reducer.ts
+++ src/features/cards/cards-reducer.ts
@@ -1,1 +1,8 @@
-export {}
+const initialState = {}
+
+export type CardsInitialStateType = typeof initialState
+
+export const cardsReducer = (state: CardsInitialStateType = initialState, action: any) => {}
+
+//Types
+// export type ActionsType = ReturnType<typeof setIsLoggedInAC>
---
+++ src/features/cards/packs-reducer.ts
@@ -0,0 +1,89 @@
+import { GetPackParams, packsAPI } from './../../api/api'
+import { AppThunk } from './../../app/store'
+import { CreateNewPackData, GetPacksResponseType, PackType } from '../../api/api'
+
+const initialState = {
+    cardPacks: <PackType[]>[
+        {
+            _id: '',
+            user_id: '',
+            user_name: '',
+            private: false,
+            name: '',
+            path: '',
+            grade: 0,
+            shots: 0,
+            deckCover: '',
+            cardsCount: 0,
+            type: '',
+            rating: 0,
+            created: null,
+            updated: null,
+            more_id: '',
+            __v: 0,
+        },
+    ],
+    page: 1,
+    pageCount: 0,
+    cardPacksTotalCount: 0,
+    minCardsCount: 0,
+    maxCardsCount: 0,
+    token: '',
+    tokenDeathTime: 0,
+}
+
+export type PacksInitialStateType = typeof initialState
+
+export const packsReducer = (state: PacksInitialStateType = initialState, action: PacksActionsType): PacksInitialStateType => {
+    switch (action.type) {
+        case 'PACKS/GET-PACKS':
+            return { ...action.packs }
+        default:
+            return state
+    }
+}
+
+//Action creators
+export const getPacksAC = (packs: GetPacksResponseType) => ({ type: 'PACKS/GET-PACKS', packs } as const)
+
+// Thunks
+export const getPacksTC =
+    (params: GetPackParams): AppThunk =>
+    (dispatch) => {
+        packsAPI
+            .getPacks(params)
+            .then((res) => {
+                dispatch(getPacksAC(res))
+            })
+            .catch((err: any) => {
+                let error = err.response.data.error
+                console.log('catch, error:', error)
+            })
+    }
+
+export const changePackNameTC =
+    (id: string, name: string, params: GetPackParams): AppThunk =>
+    (dispatch) => {
+        packsAPI.changePackName(id, name).then(() => {
+            dispatch(getPacksTC(params))
+        })
+    }
+
+export const removePackTC =
+    (id: string, params: GetPackParams): AppThunk =>
+    (dispatch) => {
+        packsAPI.removePack(id).then(() => {
+            dispatch(getPacksTC(params))
+        })
+    }
+
+export const addPackTC =
+    (newPack: CreateNewPackData, params: GetPackParams): AppThunk =>
+    (dispatch) => {
+        packsAPI.createPack(newPack).then(() => {
+            dispatch(getPacksTC(params))
+        })
+    }
+
+//Types
+export type PacksActionsType = ReturnType<typeof getPacksAC>
