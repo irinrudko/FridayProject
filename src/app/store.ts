52d87c706f8e6fe99b06f60ac0868afaf061efe4
Iryna Rudzko <irinrudko@gmail.com>
2022-10-23 19:11:22 +0400

    feat: add packsReducer and types to Store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,17 +1,16 @@
+import { packsReducer, PacksActionsType } from './../features/cards/packs-reducer'
 import { AnyAction, applyMiddleware, combineReducers, legacy_createStore } from 'redux'
 import { AppActionType, appReducer } from './app-reducer'
 import { ProfileActionType, profileReducer } from '../features/profile/profile-reducer'
 import thunk, { ThunkAction, ThunkDispatch } from 'redux-thunk'
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
-import { ActionsType, authReducer } from '../features/auth/auth-reducer'
+import { authReducer, AuthActionsType } from '../features/auth/auth-reducer'
 
 const rootReducer = combineReducers({
     app: appReducer,
     profile: profileReducer,
     auth: authReducer,
-    // registration: registrationReducer,
-    // forgotPassword: forgotPasswordReducer,
-    // login: loginReducer,
+    packs: packsReducer,
 })
 
 export const store = legacy_createStore(rootReducer, applyMiddleware(thunk))
@@ -19,7 +18,7 @@ export type AppRootStateType = ReturnType<typeof rootReducer>
 export type AppDispatch = ThunkDispatch<AppRootStateType, unknown, AnyAction>
 export const useAppDispatch = () => useDispatch<AppDispatch>()
 export const useAppSelector: TypedUseSelectorHook<AppRootStateType> = useSelector
-export type AppRootActionType = ActionsType | AppActionType | ProfileActionType
+export type AppRootActionType = AuthActionsType | AppActionType | ProfileActionType | PacksActionsType
 export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, AppRootStateType, unknown, AppRootActionType>
 
 // @ts-ignore
