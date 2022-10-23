ce78575f13ce7d9be0734aa4b3e4279dcbd4f79b
Iryna Rudzko <irinrudko@gmail.com>
2022-10-23 19:34:07 +0400

    test: add removePack

--- src/features/cards/packs-reducer.test.ts
+++ src/features/cards/packs-reducer.test.ts
@@ -1,5 +1,5 @@
 import { PackType } from '../../api/api'
-import { getPacksAC, PacksInitialStateType, packsReducer } from './packs-reducer'
+import { getPacksAC, PacksInitialStateType, packsReducer, removePackAC } from './packs-reducer'
 
 let startState: PacksInitialStateType = {
     cardPacks: <PackType[]>[
@@ -32,6 +32,56 @@ let startState: PacksInitialStateType = {
 }
 beforeEach(() => {
     startState = {
+        cardPacks: [
+            {
+                _id: '394892alsd203',
+                user_id: '7y678qjqwlqw',
+                user_name: '',
+                private: true,
+                name: '',
+                path: '',
+                grade: 0,
+                shots: 0,
+                deckCover: '',
+                cardsCount: 0,
+                type: '',
+                rating: 0,
+                created: null,
+                updated: null,
+                more_id: '',
+                __v: 0,
+            },
+            {
+                _id: '23394892alsd203hs',
+                user_id: '1212sq1211',
+                user_name: '',
+                private: false,
+                name: '',
+                path: '',
+                grade: 0,
+                shots: 0,
+                deckCover: '',
+                cardsCount: 0,
+                type: '',
+                rating: 0,
+                created: null,
+                updated: null,
+                more_id: '',
+                __v: 0,
+            },
+        ],
+        page: 2,
+        pageCount: 8,
+        cardPacksTotalCount: 2,
+        minCardsCount: 0,
+        maxCardsCount: 0,
+        token: '',
+        tokenDeathTime: 0,
+    }
+})
+
+it('should get new card packs', () => {
+    const initialState = {
         cardPacks: <PackType[]>[
             {
                 _id: '',
@@ -60,11 +110,9 @@ beforeEach(() => {
         token: '',
         tokenDeathTime: 0,
     }
-})
 
-it('should get new card packs', () => {
     const endState = packsReducer(
-        startState,
+        initialState,
         getPacksAC({
             cardPacks: [
                 {
@@ -120,4 +168,11 @@ it('should get new card packs', () => {
     expect(endState.cardPacks[0].private).toBeTruthy()
     expect(endState.cardPacks[1].private).toBeFalsy()
     expect(endState.cardPacksTotalCount).toBe(2)
+})
+
+it('should remove one pack from cardPacks colleaction', () => {
+    const endState = packsReducer(startState, removePackAC('394892alsd203'))
+
+    expect(endState.cardPacks[0]._id).toBe('23394892alsd203hs')
+    expect(endState.cardPacks[0]._id).not.toBe('394892alsd203')
 })
