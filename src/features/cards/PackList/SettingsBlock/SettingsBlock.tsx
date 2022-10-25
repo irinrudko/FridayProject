058ad2c287ff5a57376d46d5343f0caa2e4e8987
Askerko Alexandr <askerko50@gmail.com>
2022-10-25 23:52:13 +0300

    fix(filter all project)

---
+++ src/features/cards/PackList/SettingsBlock/SettingsBlock.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import s from './SettingsBlock.module.scss'
+import { Search } from '../../../../common/components/Search/Search'
+import { SliderBlock } from './SliderBlock/SliderBlock'
+import { Filter } from './Filter/Filter'
+import { getPacksTC } from '../../packs-reducer'
+
+type SettingsPropsType = {
+    setMyPack: (value: boolean) => void
+}
+
+export const SettingsBlock: React.FC<SettingsPropsType> = ({ setMyPack }) => {
+    return (
+        <div className={s.settingsBlock}>
+            <div className={s.descriptionBlock}>
+                <span>Search</span>
+                <span className={s.second}>Show packs cards</span>
+                <span className={s.third}>Number of cards</span>
+            </div>
+            <Search getThunk={getPacksTC} />
+            <Filter setMyPack={setMyPack} />
+
+            <SliderBlock />
+        </div>
+    )
+}
