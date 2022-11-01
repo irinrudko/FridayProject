5d6d938ea64a5f0f7e3cafacf6a9ad5381a0f275
Iryna Rudzko <irinrudko@gmail.com>
2022-11-01 19:14:54 +0400

    feat: add Basic modal

---
+++ src/common/components/Modals/BasicModal.tsx
@@ -0,0 +1,50 @@
+import * as React from 'react'
+import s from './BasicModal.module.scss'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import Typography from '@mui/material/Typography'
+import Modal from '@mui/material/Modal'
+
+const style = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+}
+
+type ModalType = {
+    children: React.ReactNode
+    title: string
+}
+
+export function BasicModal(props: ModalType) {
+    const [open, setOpen] = React.useState(false)
+    const handleOpen = () => setOpen(true)
+    const handleClose = () => setOpen(false)
+
+    return (
+        <div className={s.item}>
+            <Button type={'button'} variant={'contained'} color={'primary'} className={s.button} onClick={handleOpen}>
+                {props.title}
+            </Button>
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style}>
+                    <Typography id="modal-modal-title" variant="h6" component="h2">
+                        {props.title}
+                    </Typography>
+                    <Button onClick={handleClose}>Cancel</Button>
+                </Box>
+            </Modal>
+        </div>
+    )
+}
