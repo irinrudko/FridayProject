af9625bbc2134fb81b6766b7236612c36b54b32d
Iryna Rudzko <irinrudko@gmail.com>
2022-11-03 19:55:53 +0400

    feat: add RemoveCardModal hook

--- src/common/components/Modals/useModal.ts
+++ src/common/components/Modals/useModal.ts
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 
 export const useModal = () => {
+    ///packs
     const [addPackModal, setAddPackModal] = useState(false)
     const [editPackModal, setEditPackModal] = useState(false)
     const [removePackModal, setRemovePackModal] = useState(false)
@@ -15,12 +16,21 @@ export const useModal = () => {
         setRemovePackModal(!removePackModal)
     }
 
+    ////cards
+    const [removeCardModal, setRemoveCardModal] = useState(false)
+
+    function toggleRemoveCardModal() {
+        setRemoveCardModal(!removeCardModal)
+    }
+
     return {
         addPackModal,
         editPackModal,
         removePackModal,
+        removeCardModal,
         toggleAddPackModal,
         toggleEditPackModal,
         toggleRemovePackModal,
+        toggleRemoveCardModal,
     }
 }
