8fb81db92cb6325e8255660a7d5cfb84ce01ffad
Iryna Rudzko <irinrudko@gmail.com>
2022-11-03 16:28:34 +0400

    fix: add React Portal for Basic Modal

---
+++ src/common/components/Modals/BasicModalWithPortal.tsx
@@ -0,0 +1,64 @@
+import * as React from 'react'
+import ReactDOM from 'react-dom'
+import s from './BasicModal.module.scss'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import Typography from '@mui/material/Typography'
+
+const style = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 395,
+    height: 311,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+}
+
+type ModalType = {
+    children: React.ReactNode
+    title: string
+    onSaveClick: () => void
+
+    isShowing: boolean
+    hide: () => void
+}
+
+export const BasicModalWithPortal: React.FC<ModalType> = ({ isShowing, hide, children, title, onSaveClick }) =>
+    isShowing
+        ? ReactDOM.createPortal(
+              <>
+                  <div className={s.item}>
+                      <Box sx={style}>
+                          <Typography id="modal-modal-title" variant="h6" component="h2" className={s.title}>
+                              {title}
+                          </Typography>
+                          {children}
+                          <div className={s.buttonContainer}>
+                              <Button onClick={hide} variant={'contained'} color={'inherit'} className={s.button}>
+                                  Cancel
+                              </Button>
+                              <Button
+                                  onClick={() => {
+                                      onSaveClick()
+                                      hide()
+                                  }}
+                                  variant={'contained'}
+                                  color={'primary'}
+                                  className={`${s.button} ${s.buttonPrimary}`}
+                              >
+                                  Save
+                              </Button>
+                          </div>
+                      </Box>
+                  </div>
+              </>,
+              document.body
+          )
+        : null
