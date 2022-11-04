77830e4b32849530b73c5688b1f0bcdb35eaf0fb
Iryna Rudzko <irinrudko@gmail.com>
2022-11-04 09:18:56 +0400

    fix: add Modal from MUI for styles

--- src/common/components/Modals/BasicModalWithPortal.tsx
+++ src/common/components/Modals/BasicModalWithPortal.tsx
@@ -4,6 +4,7 @@ import s from './BasicModal.module.scss'
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography'
+import Modal from '@mui/material/Modal'
 
 const style = {
     position: 'absolute' as 'absolute',
@@ -32,38 +33,46 @@ type ModalType = {
 }
 
 //TODO:
-//exit onBlur
 //memoization
 
-export const BasicModal: React.FC<ModalType> = ({ isShowing, hide, children, title, onSaveClick, buttonTitle }) =>
-    isShowing
+export const BasicModal: React.FC<ModalType> = ({ isShowing, hide, children, title, onSaveClick, buttonTitle }) => {
+    const onSave = () => {
+        onSaveClick()
+        hide()
+    }
+    return isShowing
         ? ReactDOM.createPortal(
               <>
                   <div className={s.item}>
-                      <Box sx={style}>
-                          <Typography id="modal-modal-title" variant="h6" component="h2" className={s.title}>
-                              {title}
-                          </Typography>
-                          {children}
-                          <div className={s.buttonContainer}>
-                              <Button onClick={hide} variant={'contained'} color={'inherit'} className={s.button}>
-                                  Cancel
-                              </Button>
-                              <Button
-                                  onClick={() => {
-                                      onSaveClick()
-                                      hide()
-                                  }}
-                                  variant={'contained'}
-                                  color={'primary'}
-                                  className={`${s.button} ${s.buttonPrimary}`}
-                              >
-                                  {buttonTitle || 'Save'}
-                              </Button>
-                          </div>
-                      </Box>
+                      <Modal
+                          open={isShowing}
+                          onClose={hide}
+                          aria-labelledby="modal-modal-title"
+                          aria-describedby="modal-modal-description"
+                      >
+                          <Box sx={style}>
+                              <Typography id="modal-modal-title" variant="h6" component="h2" className={s.title}>
+                                  {title}
+                              </Typography>
+                              {children}
+                              <div className={s.buttonContainer}>
+                                  <Button onClick={hide} variant={'contained'} color={'inherit'} className={s.button}>
+                                      Cancel
+                                  </Button>
+                                  <Button
+                                      onClick={onSave}
+                                      variant={'contained'}
+                                      color={'primary'}
+                                      className={`${s.button} ${s.buttonPrimary}`}
+                                  >
+                                      {buttonTitle || 'Save'}
+                                  </Button>
+                              </div>
+                          </Box>
+                      </Modal>
                   </div>
               </>,
               document.body
           )
         : null
+}
