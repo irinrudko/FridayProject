e9b664054f8d7f52fe959980caa2045ad7bacbef
Iryna Rudzko <irinrudko@gmail.com>
2022-11-11 21:01:52 +0400

    refactor: use UploadPhoto component

--- src/features/modals/packsModals/AddPackModal.tsx
+++ src/features/modals/packsModals/AddPackModal.tsx
@@ -1,9 +1,9 @@
-import React, { ChangeEvent, useState } from 'react'
-import { Button, Checkbox, FormControlLabel, FormGroup, TextField } from '@mui/material'
+import React, { useState } from 'react'
+import { Checkbox, FormControlLabel, FormGroup, TextField } from '@mui/material'
 import { useAppDispatch } from '../../../app/store'
 import { addPackTC } from '../../cards/PackList/packs-reducer'
 import { BasicModal } from '../../../common/components/Modal/BasicModal'
-import { setErrAC } from '../../../app/app-reducer'
+import UploadPhoto from '../cardsModals/UploadPhoto/UploadPhoto'
 
 type AddPackModalType = {
     title: string
@@ -12,11 +12,11 @@ type AddPackModalType = {
 }
 
 export const AddPackModal = (props: AddPackModalType) => {
-    let avatar: string
     const dispatch = useAppDispatch()
 
     let [name, setName] = useState('')
     let [isPrivate, setIsPrivate] = useState(false)
+    const [uploadPhoto, setUploadPhoto] = React.useState<string>('')
 
     const setNameHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
         setName(e.currentTarget.value)
@@ -26,34 +26,11 @@ export const AddPackModal = (props: AddPackModalType) => {
         setIsPrivate(e.currentTarget.checked)
     }
 
-    const uploadAvatar = (e: ChangeEvent<HTMLInputElement>) => {
-        if (e.target.files && e.target.files.length) {
-            const file = e.target.files[0]
-
-            if (file.size < 1048576) {
-                convertFileToBase64(file, (file64: string) => {
-                    avatar = file64
-                })
-            } else {
-                dispatch(setErrAC('Error: File size is too big. Max pic size is 1mb'))
-            }
-        }
-    }
-
-    const convertFileToBase64 = (file: File, callBack: (value: string) => void) => {
-        const reader = new FileReader()
-        reader.onloadend = () => {
-            const file64 = reader.result as string
-            callBack(file64)
-        }
-        reader.readAsDataURL(file)
-    }
-
     const addNewPack = () => {
         const newPack = {
             cardsPack: {
                 name: name,
-                deckCover: avatar,
+                deckCover: uploadPhoto,
                 private: isPrivate,
             },
         }
@@ -64,12 +41,7 @@ export const AddPackModal = (props: AddPackModalType) => {
     return (
         <BasicModal title={props.title} onSaveClick={addNewPack} isShowing={props.isShowing} hide={props.hide}>
             <TextField variant="standard" label="Name pack" value={name} onChange={setNameHandler} />
-            <label>
-                <input type="file" onChange={uploadAvatar} style={{ display: 'none' }} />
-                <Button variant="contained" component="span">
-                    Upload pack avatar
-                </Button>
-            </label>
+            <UploadPhoto setUploadPhoto={setUploadPhoto} uploadPhoto={uploadPhoto} />
             <FormGroup>
                 <FormControlLabel control={<Checkbox onChange={setIsPrivateHandler} />} label="Private pack" />
             </FormGroup>
